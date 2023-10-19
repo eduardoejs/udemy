cd2ecf014c329bd6ee56c1d77fe238238780e32b
Eduardo Silva <eduardo.ejs@hotmail.com>
2023-10-19 15:49:38 -0300

    excluindo arquivos antigos

--- web-moderno-js/section-04/fundamentos/hoisting.js
+++
@@ -1,5 +0,0 @@
-console.log('a = ', a);
-var a = 2
-console.log('a = ', a);
-
-console.log();
--- web-moderno-js/section-04/fundamentos/usandoVar_2.js
+++
@@ -1,8 +0,0 @@
-var numero = 1
-
-{
-    var numero = 2
-    console.log('dentro do bloco de codigo = ', numero);
-}
-
-console.log('fora do bloco de codigo = ', numero);
